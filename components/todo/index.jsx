52f7805ff8c7eab86e3b3ead403ad593f09536f4
SergMt <66214453+Serg-Mt@users.noreply.github.com>
2025-03-06 22:35:25 +0400

    + оптимизация

--- components/todo/index.jsx
+++ components/todo/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, memo, useCallback, useRef } from 'react';
 
 class ToDoItem {
   id = Math.random();
@@ -7,30 +7,78 @@ class ToDoItem {
   constructor(text) {
     this.text = text;
   }
+  clone() {
+    const
+      result = new ToDoItem;
+    Object.assign(result, this);
+    return result;
+  }
   toggle() {
     this.completed = !this.completed;
-    return this;
+    return this.clone();
   }
 }
+const Button = memo(function ({ children, onClick }) {
+  console.debug('Button', children);
+  return <button onClick={onClick}>{children}</button>;
+});
+
+const Item = memo(
+  /**
+   * 
+   * @param {object} props 
+   * @param {ToDoItem} props.item
+   * @returns {JSX.Element}
+   */
+  function ({ item, delItem, toggleItem }) {
+    console.debug('Item', item);
+    const
+      { id, text, completed } = item,
+      onClick = useCallback(() => delItem(id), [id]);
+
+    return <li>
+      <input type="checkbox" checked={completed} onChange={() => toggleItem(id)} />
+      {completed && '✔'}
+      {text}
+      <Button onClick={onClick}>✖</Button>
+    </li>
+  }
+);
+
+const Form = memo(
+  function ({ addItem }) {
+    const
+      ref = useRef(null),
+      // [value, setValue] = useState('=default='),
+      onClick = useCallback(() => { addItem(ref.current.value); ref.current.value = '' }, []);
+    console.debug('Form', { ref });
+    return <>
+      <input ref={ref} />
+      <Button onClick={onClick}>➕</Button >
+    </>
+  }
+);
 
 export function ToDoList() {
   const
     [list, setList] = useState([
       new ToDoItem('купить молока'),
       new ToDoItem('покормить кошку')]),
-    addItem = text => setList(prev => prev.concat(new ToDoItem(text))),
-    delCompleted = () => setList(prev => prev.filter(elem => !elem.completed)),
-    toggleItem = id => setList(prev => {
-      console.log('toggleItem', { prev })
+    addItem = useCallback(text => setList(
+      prev => prev.concat(new ToDoItem(text))), []),
+    delCompleted = useCallback(() => setList(
+      prev => prev.filter(elem => !elem.completed)), []),
+    toggleItem = useCallback(id => setList(prev => {
+      // console.log('toggleItem', { prev })
       const
         index = prev.findIndex(elem => id === elem.id),
-        elem = prev[index];
-      elem.toggle();
+        elem = prev[index].toggle();
+      // elem.toggle();
       // console.log('toggleItem', { id, prev, index })
       return prev.with(index, elem);
-    }),
-
-    delItem = id => setList(prev => prev.filter(elem => id !== elem.id));
+    }), []),
+    delItem = useCallback(id => setList(
+      prev => prev.filter(elem => id !== elem.id)), []);
   return <>
     <h3>ToDo List</h3>
     <Form addItem={addItem} />
@@ -40,41 +88,10 @@ export function ToDoList() {
 }
 
 
-
 function List({ list, delItem, toggleItem }) {
+  console.debug('List');
   return <ol>
     {list.map(item =>
       <Item key={item.id} item={item} delItem={delItem} toggleItem={toggleItem} />)}
   </ol>
-}
-
-function Form({ addItem }) {
-  const
-    [value, setValue] = useState('');
-  return <>
-    <input value={value} onInput={event => setValue(event.target.value)} />
-    <Button onClick={() => { addItem(value); setValue('') }}>➕</Button>
-  </>
-}
-
-/**
- * 
- * @param {object} props 
- * @param {ToDoItem} props.item
- * @returns {JSX.Element}
- */
-function Item({ item, delItem, toggleItem }) {
-  const
-    { id, text, completed } = item;
-
-  return <li>
-    <input type="checkbox" value={completed} onChange={() => toggleItem(id)} />
-    {completed && '✔'}
-    {text}
-    <Button onClick={() => delItem(id)}>✖</Button>
-  </li>
-}
-
-function Button({ children, onClick }) {
-  return <button onClick={onClick}>{children}</button>;
 }
